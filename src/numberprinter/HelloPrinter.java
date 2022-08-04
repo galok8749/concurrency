0126fd74c6660c2cd9248ade6e9dba64c729e192
alok <galok8749@gmail.com>
2022-08-04 18:28:14 +0530

    print hello using seperate thread

---
+++ src/numberprinter/HelloPrinter.java
@@ -0,0 +1,8 @@
+package numberprinter;
+
+public class HelloPrinter implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Hello");
+    }
+}
--- src/numberprinter/Main.java
+++ src/numberprinter/Main.java
@@ -3,7 +3,11 @@ package numberprinter;
 public class Main {
     public static void main(String[] args) {
         for(int i = 0; i < 100; ++i) {
-            System.out.println("Number is " + i);
+            Thread t = new Thread(new HelloPrinter());
+//            calling start method instead run , if we directly call run it will not create thread
+//            start internally going to call run, start will create new memory for the thread
+            t.start();
+//            System.out.println("Number is " + i);
         }
     }
 }
