ef4ec2df2c7334659522640c19f55a670e39ae24
alok <galok8749@gmail.com>
2022-08-04 18:20:48 +0530

    INITIAL COMMIT

---
+++ src/numberprinter/Main.java
@@ -0,0 +1,9 @@
+package numberprinter;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; ++i) {
+            System.out.println("Number is " + i);
+        }
+    }
+}
